e98180a45f8d74c68967c52da10e7e9c04d87dbc
Angatia Benson <bensonetia@gmail.com>
2025-01-19 17:31:57 +0300

    feat: add song model and migrations,  controllers, dtos and mappers.

---
+++ Controllers/SongController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicAPI.Data;
+using MusicAPI.Mappers;
+
+namespace MusicAPI.Controllers
+{
+    [Route("api/song")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public SongController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var songs = _context.Songs.ToList().Select(s => s.ToSongDto());
+            return Ok(songs);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var song = _context.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            return Ok(song.ToSongDto());
+        }
+    }
+}
---
+++ Mappers/SongMappers.cs
@@ -0,0 +1,20 @@
+﻿using MusicAPI.Dtos;
+using MusicAPI.Models;
+
+namespace MusicAPI.Mappers
+{
+    public static class SongMappers
+    {
+        public static SongDto ToSongDto(this Song song)
+        {
+            return new SongDto
+            {
+                Title = song.Title,
+                Artist = song.Artist,
+                Album = song.Album,
+                Genre = song.Genre,
+                Year = song.Year
+            };
+        }
+    }
+}
